55de6b167446d5c152141d82319ac66bf9f318bb
sarahchaaaaa <sarahhwang711@gmail.com>
2019-12-05 16:38:03 -0500

    refactored App.js

--- pokedex-blog/src/components/App.js
+++ pokedex-blog/src/components/App.js
@@ -1,36 +1,15 @@
+// App.js
+
 import React, { Component } from 'react';
-import PokeList from './PokeList';
-import DetailView from './DetailView';
-import Pokemon from '../Pokemon';
+// import Button from '@material-ui/core/Button';
+// import Bookmarks from '@material-ui/icons/Bookmarks';
 import './styles/App.css';
+import PokeApp from './PokeApp';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pokemon: {}
-    };
-
-    this.handleOnClick = this.handleOnClick.bind(this);
-  }
-
-  handleOnClick(id) {
-    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
-      .then(res => res.json())
-      .then(data => {
-        const pokemon = new Pokemon(data);
-
-        this.setState({ pokemon });
-      })
-      .catch(err => console.log(err));
-  }
-
   render() {
     return (
-      <div className="App">
-        <PokeList handleOnClick={this.handleOnClick} />
-        <DetailView pokemon={this.state.pokemon} />
-      </div>
+        <PokeApp/>
     );
   }
 }
