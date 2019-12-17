afffce7f39147df70f4311a3176cd32d8350f41c
Johnnn <jhoeksem@nd.edu>
2019-12-17 08:59:43 -0500

    adding code for home page to website; Imari it looks stellar

---
+++ frontend/scripts/home.js
@@ -0,0 +1,119 @@
+// CONSTANTS 
+PORT_NUM = 51053;
+BASE_URL = "http://student04.cse.nd.edu:" + PORT_NUM; 
+IMG_URL = "http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+TYPE_URL = "http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/"; 
+
+// PROTOTYPES 
+Label.prototype = new Item(); 
+Button.prototype = new Item(); 
+Div.prototype = new Item(); 
+Image.prototype = new Item(); 
+SearchBar.prototype = new Item(); 
+
+// ELEMENTS TO DISPLAY ON PAGE
+var title = new Label(); 
+title.createLabel("WELCOME TO POKEAPI", "title");
+
+var strongTypes = new Label(); 
+strongTypes.createLabel("", "strongTypes");
+strongTypes.addToDocument(); 
+
+var strongLabel = new Label(); 
+strongLabel.createLabel("Strengths: ", "strongLabel");
+
+var weakTypes = new Label();
+weakTypes.createLabel("", "weakTypes"); 
+
+var weakLabel = new Label(); 
+weakLabel.createLabel("Weaknesses: ", "weakLabel");
+
+var type = new Label();
+type.createLabel("", "TYPE"); 
+
+var typeLabel = new Label(); 
+typeLabel.createLabel("Type: ", "typeLabel");
+
+var description2 = new Label(); 
+description2.createLabel(" ", "description2");  
+description2.addToDocument(); 
+
+var description = new Label(); 
+description.createLabel("The functionality of this API is to help a beginner at Pokemon with best and worst matchups. The user will be able to enter their Pokemon of choice, and our program will send requests to our API to return all of the type matchups of the given Pokemon and both a good and a poor matchup against it.", ""); 
+
+var sprite = new Image();
+var startImg = "https://img.icons8.com/plasticine/100/000000/pokeball.png";
+sprite.createImage("sprite", startImg);
+
+var typeSprite = new Image();
+var typeStartImg = IMG_URL + "0.png";
+typeSprite.createImage("typeSprite", typeStartImg); 
+
+var strongSprite = new Image();
+var strongStartImg = IMG_URL + "0.png";
+strongSprite.createImage("strongSprite", strongStartImg); 
+
+var weakSprite = new Image();
+var weakStartImg = IMG_URL + "0.png";
+weakSprite.createImage("weakSprite", weakStartImg); 
+
+var weakAgainstLabel = new Label();
+weakAgainstLabel.createLabel("", "weakAgainstLabel");
+
+var weakAgainstType = new Label();
+weakAgainstType.createLabel("", "weakAgainstType");
+
+var strongAgainstLabel = new Label();
+strongAgainstLabel.createLabel("", "strongAgainstLabel");
+
+var strongAgainstType = new Label();
+strongAgainstType.createLabel("", "strongAgainstType");
+
+
+// DIVS 
+var titleDiv = new Div();
+titleDiv.createDiv("titleDiv", "titleDiv");
+titleDiv.add(description);
+titleDiv.add(title);
+
+var weakAgainst = new Div();
+weakAgainst.createDiv("weakAgainst", "weakAgainst");
+weakAgainst.add(weakAgainstLabel);
+weakAgainst.add(weakAgainstType);
+
+var strongAgainst = new Div();
+strongAgainst.createDiv("strongAgainst", "strongAgainst");
+strongAgainst.add(strongAgainstLabel);
+strongAgainst.add(strongAgainstType);
+
+var weakDiv = new Div(); 
+weakDiv.createDiv("weakDiv", "weakDiv");
+weakDiv.add(weakLabel); 
+weakDiv.add(weakTypes);
+weakDiv.add(weakAgainst);
+weakDiv.add(weakSprite);
+
+var strongDiv = new Div(); 
+strongDiv.createDiv("strongDiv", "strongDiv"); 
+strongDiv.add(strongLabel); 
+strongDiv.add(strongTypes);
+strongDiv.add(strongAgainst);
+strongDiv.add(strongSprite);
+
+var typeDiv = new Div();
+typeDiv.createDiv("typeDiv", "typeDiv"); 
+typeDiv.add(typeLabel); 
+typeDiv.add(type); 
+typeDiv.add(typeSprite);
+
+var headerContainer = new Div();
+headerContainer.createDiv("headerContainer", "headerContainer");
+headerContainer.add(titleDiv);
+headerContainer.add(sprite);
+headerContainer.addToDocument();
+
+var infoContainer = new Div(); 
+infoContainer.createDiv("infoContainer", "infoContainer"); 
+infoContainer.add(description);
+infoContainer.add(description2);
+infoContainer.addToDocument(); 
